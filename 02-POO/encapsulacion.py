d12036fa0f5c74ccd9b69daf5f2f840258c4cf49
Alejandrina Lara <tovrale3ds@gmail.com>
2020-08-13 20:41:10 +0900

    Codigo con falla

---
+++ 02-POO/encapsulacion.py
@@ -0,0 +1,22 @@
+#No corre bien el codigo 
+class CasillaDeVotacion:
+    
+    def __init__(self, identificador, pais):
+        self._identificador = identificador
+        self._pais = pais
+        self._region = None
+
+    @property
+    def region(self):
+        return self._region
+
+    @region.setter
+    def set_region(self, region):
+        if region in self._pais:
+            self._region = region
+        else:
+            raise ValueError(f'La region {region} no esta en la lista {self._pais}')
+
+if __name__=='__main__':
+	voto = CasillaDeVotacion()
+    voto.region()
