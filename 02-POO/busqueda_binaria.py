9608642a98ebc24660d0a192f6db7cc7eca08b13
Alejandrina Lara <tovrale3ds@gmail.com>
2020-08-18 21:20:22 +0900

    se cambio el valor del indice final

--- 02-POO/busqueda_binaria.py
+++ 02-POO/busqueda_binaria.py
@@ -1,13 +1,15 @@
 import random
 
 def busqueda_binaria(lista,comienzo, final, objetivo):
-
+    print(f'comienzo {comienzo}, final: {final}, objetivo: {objetivo}')
+    #print(f'buscando {objetivo} entre {lista[comienzo]} y {lista[final-1]}')
     if comienzo > final:
         return False
     
     medio = (comienzo + final)//2
+    print(f'medio: {medio}')
 
-    if lista[medio] < objetivo:
+    if lista[medio] == objetivo:
         return True
     elif lista[medio]< objetivo:
         return busqueda_binaria(lista, medio+1, final, objetivo)
@@ -21,7 +23,6 @@ if __name__=='__main__':
     objetivo = int(input('Que numero quieres encontrar ? '))
 
     lista = sorted([random.randint(0,100) for i in range(tamano_de_lista)])
-
-    encontrado = busqueda_binaria(lista, 0 , len(lista), objetivo)
     print(lista)
+    encontrado = busqueda_binaria(lista, 0, len(lista) - 1, objetivo)
     print(f'El elemento {objetivo}{" esta" if encontrado else " no esta"} en la lista')
